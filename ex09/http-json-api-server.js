14d401e7a67a138852575cbab0a1b65b75cda7c0
monoue <like_a_newborn_baby@icloud.com>
2021-01-05 16:22:02 +0900

    refactoring

--- ex09/http-json-api-server.js
+++ ex09/http-json-api-server.js
@@ -1,47 +1,76 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   time-server.js                                     :+:      :+:    :+:   */
+/*   http-json-api-server.js                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: monoue <monoue@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/28 10:39:10 by monoue            #+#    #+#             */
-/*   Updated: 2020/12/29 09:52:23 by monoue           ###   ########.fr       */
+/*   Updated: 2021/01/05 16:21:54 by monoue           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 'use strict'
 
 {
-	const arg_error = 'Error: Arg num is not one';
+	const error = 'Error: ';
+	const arg_num_error = `${error}Arg num is not one`;
+	const arg_content_error = `${error}Invalid arg format`;
+	const query_num_error = `${error}Num of queries should be one`;
+	const query_key_error = `${error}Key of query must be 'iso'`;
 
-	if (process.argv.length != 3)
-		return console.log(arg_error);
+	const parsetime_path = '/api/parsetime'
+	const unixtime_path= '/api/unixtime'
 
-	const format = num => num.toString().padStart(2, '0');
+	if (process.argv.length !== 3)
+		return console.log(arg_num_error);
 
-	const getCurrentDateAndTime = () => {
-		const date = new Date();
-		const year = date.getFullYear().toString();
-		const month = format(date.getMonth() + 1);
-		const day = format(date.getDate());
-		const hour = format(date.getHours());
-		const minute = format(date.getMinutes());
+	const http = require('http');
+	const URL = require('url').URL;
+	const port = parseInt(process.argv[2], 10);
 
-		return `${year}-${month}-${day} ${hour}:${minute}\n`;
+	const get
+
+	const getJSONTime = (pathname, date) => {
+		if (pathname === parsetime_path)
+			return getJSONParsedTime(date);
+		else
+			return getJSONUnixTime(date);
 	};
 
-	try {
-		const server = require('net').createServer(socket => {
-			socket.on("error", () => {
-				return;
-			});
-			socket.end(getCurrentDateAndTime());
-		});
-		server.on("error", () => {
-			return;
-		});
-		server.listen(parseInt(process.argv[2]));
-	} catch {
-	};
+	const server = http.createServer((req, res) => {
+		const urlObj = new URL(`http://localhost:${port}${req.url}`);
+		const queryData = urlObj.searchParams;
+		const isoString = queryData.get('iso');
+
+		if (isoString === undefined)
+			return console.log(query_key_error);
+
+		const pathname = urlObj.pathname;
+
+		if (pathname === parsetime_path || pathname === unixtime_path)
+		{
+			res.writeHead(200, {'Content-Type': 'application/json'});
+			const date = new Date(isoString);
+			let arr;
+			if (pathname === parsetime_path)
+			{
+				arr = {
+					hour: date.getUTCHours(),
+					minute: date.getUTCMinutes(),
+					second: date.getUTCSeconds()
+				};
+			}
+			else
+				arr = {unixtime: date.getTime()};
+			res.end(JSON.stringify(arr));
+		}
+		else
+		{
+			res.writeHead(404);
+			res.end('404 not found');
+		}
+	});
+
+	server.listen(port);
 }
